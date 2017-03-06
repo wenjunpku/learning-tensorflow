d111cf9762b3cd97fef0630e7876dd532d1e202e
Wenjun Wang <wenjunwang@pku.edu.cn>
2017-03-06 17:31:44 +0800

    remove test part

--- test.py
+++ test.py
@@ -43,6 +43,3 @@ new_samples = np.array(
     [[6.4, 3.2, 4.5, 1.5], [5.8, 3.1, 5.0, 1.7]], dtype=float)
 y = list(classifier.predict(new_samples, as_iterable=True))
 print('Predictions: {}'.format(str(y)))
-#test github
-#test midified
-#third
